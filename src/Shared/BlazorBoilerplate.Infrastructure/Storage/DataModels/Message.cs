90c3cddf9f85bdca42361fe1b0fbeed0c2367673
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-10-20 00:47:36 -0300

    Initial commit

---
+++ src/Shared/BlazorBoilerplate.Infrastructure/Storage/DataModels/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBoilerplate.Infrastructure.Storage.DataModels
+{
+    public partial class Message
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "FieldRequired")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "FieldRequired")]
+        public string Text { get; set; }
+        public DateTime When { get; set; }
+        public Guid UserID { get; set; }
+        public ApplicationUser Sender { get; set; }
+    }
+}
